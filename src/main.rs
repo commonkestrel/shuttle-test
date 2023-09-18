294a7d46a077bcb0d62bf1cfd2ef907574a6c1df
Kestrel <jettb6@gmail.com>
2023-09-18 00:18:23 -0700

    Actually use `actix-files`

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
 use actix_web::{get, HttpResponse, HttpRequest, Responder, web::{self, ServiceConfig}};
 use shuttle_actix_web::ShuttleActixWeb;
-use std::{io::ErrorKind, fs, path::Path};
+use std::{io::ErrorKind, path::Path};
 
 #[get("/")]
 async fn hello_world() -> impl Responder {
     HttpResponse::Ok().body("Yippee")
-}
-
-#[get("/static/{path}")]
-async fn serve_static(req: HttpRequest, path: web::Path<String>) -> HttpResponse {
-    let f = match actix_files::NamedFile::open_async(Path::new("static").join(&path.into_inner())).await {
-        Ok(f) => f,
-        Err(err) => return match err.kind() {
-            ErrorKind::NotFound => HttpResponse::NotFound().into(),
-            ErrorKind::InvalidInput => HttpResponse::BadRequest().into(),
-            _ => HttpResponse::InternalServerError().into(),
-        }
-    };
-
-    f.into_response(&req)
 }
 
 #[shuttle_runtime::main]
@@ -26,7 +12,7 @@ async fn actix_web(
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
-        cfg.service(serve_static);
+        cfg.service(actix_files::Files::new("/static", "./static").show_files_listing());
     };
 
     Ok(config.into())
